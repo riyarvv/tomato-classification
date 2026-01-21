3e09c9daa585408b44949c7854f3cad962533106
Riya R V <riyarv7723@gmail.com>
2026-01-21 10:28:53 +0530

    Modify GRIPPER_CH angles for stability
    Updated GRIPPER_CH values to prevent stalling.

--- test_slow_2.py
+++ test_slow_2.py
@@ -32,7 +32,7 @@ LIMITS = {
     PITCH_CH:    {"neutral": 90,  "min": 40,  "max": 120},
     SHOULDER_CH: {"neutral": 130, "pick": 115},
     ELBOW_CH:    {"neutral": 65,  "pick": 100},
-    GRIPPER_CH:  {"open": 180,    "close": 10} # Changed 0 to 10 to prevent stalling
+    GRIPPER_CH:  {"open": 160,    "close": 30} # Changed 0 to 10 to prevent stalling
 }
 
 def move_slow(channel_id, target_angle, speed=0.04):
