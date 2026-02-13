c60797140b395046106f0e4c891bc9c8d8b110ab
Riya R V <riyarv7723@gmail.com>
2026-02-13 13:52:20 +0530

    Update center_detect.py

--- center_detect.py
+++ center_detect.py
@@ -25,7 +25,7 @@ for ch in channels:
 LIMITS = {
     BASE_CH:     {"neutral": 20,  "min": 10,  "max": 50},
     PITCH_CH:    {"neutral": 90,  "min": 40,  "max": 120},
-    SHOULDER_CH: {"neutral": 130, "pick": 115},
+    SHOULDER_CH: {"neutral": 125, "pick": 115},
     ELBOW_CH:    {"neutral": 30,  "pick": 50},
     GRIPPER_CH:  {"open": 170,    "close": 20}
 }
