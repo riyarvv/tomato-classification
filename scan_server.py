e5941de2811e011fe84fecb6b4a60d3d9652a773
Riya R V <riyarv7723@gmail.com>
2026-03-09 11:54:44 +0530

    Change serial port from '/dev/ttyACM0' to '/dev/esp32'

--- scan_server.py
+++ scan_server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # ================================
 # SERIAL CONNECTION (ESP32)
 # ================================
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+ser = serial.Serial('/dev/esp32', 115200, timeout=1)
 
 # ================================
 # GLOBAL VARIABLES
