c8c3d803e53a9f9e44e9d0556bce6e34fcb219c2
sreevidyaharidas <sreevidyah2004@gmail.com>
2026-03-09 16:12:19 +0530

    Update scan_server.py

--- scan_server.py
+++ scan_server.py
@@ -202,7 +202,10 @@ def pick():
     if scan_process is None or scan_process.poll() is not None:
 
         print("Starting scan_pick.py ...")
-        scan_process = subprocess.Popen(["python3","scan_pick.py"])
+        scan_process = subprocess.Popen([
+"/home/rslvpi5/tomato-detection/tomato-classification/venv/bin/python",
+"scan_pick.py"
+])
         
         harvesting = True
 
