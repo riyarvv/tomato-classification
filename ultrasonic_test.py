73247de9b74ae50a437327054fe76c23c64248c0
Riya R V <riyarv7723@gmail.com>
2026-03-19 14:10:40 +0530

    Create ultrasonic_test.py

---
+++ ultrasonic_test.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+TRIG = 23
+ECHO = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+time.sleep(2)
+
+def get_distance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    duration = pulse_end - pulse_start
+    distance = duration * 17150  # cm
+
+    return distance
+
+def get_stable_distance():
+    readings = []
+    for _ in range(5):
+        readings.append(get_distance())
+        time.sleep(0.05)
+    return sum(readings)/len(readings)
+
+try:
+    while True:
+        print("Distance:", get_stable_distance())
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
