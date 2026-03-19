b0c4852f584b72610e34df05ab55332b3adc0d3b
Riya R V <riyarv7723@gmail.com>
2026-03-19 14:16:31 +0530

    Update ultrasonic_test.py

--- ultrasonic_test.py
+++ ultrasonic_test.py
@@ -12,18 +12,27 @@ GPIO.output(TRIG, False)
 time.sleep(2)
 
 def get_distance():
+
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
     GPIO.output(TRIG, False)
 
+    start_time = time.time()
+
+    # wait for echo start
     while GPIO.input(ECHO) == 0:
         pulse_start = time.time()
+        if pulse_start - start_time > 0.02:
+            return -1   # timeout
 
+    # wait for echo end
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
+        if pulse_end - pulse_start > 0.02:
+            return -1   # timeout
 
     duration = pulse_end - pulse_start
-    distance = duration * 17150  # cm
+    distance = duration * 17150
 
     return distance
 
