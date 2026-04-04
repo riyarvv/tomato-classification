c29d6727bb9eb4938ba0a0e16064bc2b78b07dbb
Riya R V <riyarv7723@gmail.com>
2026-04-04 12:00:48 +0530

    Change increment URL from localhost to IP address

--- pluck.py
+++ pluck.py
@@ -190,7 +190,7 @@ while True:
                   # 🍅 COUNT TRIGGER
                   if g == 30:
                       try:
-                          requests.get("http://localhost:5001/increment")
+                          requests.get("http://10.215.117.125:5001/increment")
                           print("🍅 Count increment sent")
                       except:
                           print("⚠️ Server not reachable")
